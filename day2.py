0047939d8f1f03b65581bf6bf2c6842bd4983871
shreyasicodes20 <97606334+shreyasicodes20@users.noreply.github.com>
2022-02-11 23:21:22 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,54 @@
+#arithmetic Operators
+# +, -, *, /, //, %, **
+ 
+a=3
+b=5
+print(b**a) #5x5x5
+ 
+# Assignment Operators
+# =, +=, -=, *=, /=, //=, %=, **=
+ 
+a = 5
+a *= 2
+print(a)
+ 
+#Comparison Operators
+# ==, !=, >, <, >=, <=
+a=7
+b=6
+if a != b:
+ print("Yes")
+else:
+ print("no")
+ 
+#Logical Operators
+# and, or, not
+a=3
+b=5
+c=8
+d=7
+if a>b or c>d :
+ print("yes")
+else:
+ print("no")
+ 
+#Identity Operators
+# is, is not
+a=5
+b=6
+if a is not b:
+ print('Yes')
+ 
+else:
+ print('No')
+ 
+#Membership Operators
+# in, not in
+ 
+array1=[1,2,3,4,5]
+a=5
+if a not in array1:
+ print('Yes')
+else:
+ print('No')
+
